7a618f9abd376fd03c0517b801442bda1c2e3910
Ronny26 <priyanshushrivastava786@gmail.com>
2023-11-20 18:50:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+import cors from 'cors'
+import Hello from './hello.js'
+import CourseRoutes from './courses/routes.js'
+import Lab5 from './lab5.js'
+import ModuleRoutes from './Modules/routes.js'
+import 'dotenv/config'
+const app = express()
+app.use(cors())
+CourseRoutes(app)
+app.use(express.json())
+ModuleRoutes(app)
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000)
